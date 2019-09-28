ee19f959a0ed5fca722cf4c81d9398627c57c394
Pawee Kittiwathanakul <boatmind2710@gmail.com>
2019-09-28 18:25:47 +0700

    Add CollectionCell

---
+++ AccountNumberLucky/cell/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  AccountNumberLucky
+//
+//  Created by Pawee Kittiwathanakul on 28/9/2562 BE.
+//  Copyright © 2562 Pawee Kittiwathanakul. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
